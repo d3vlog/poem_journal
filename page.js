7c8fbfd78b182cd76f6a9eeb2d35fc4b8393c660
d3vlog <63188820+d3vlog@users.noreply.github.com>
2022-10-06 20:34:00 +0200

    Update page.js

--- page.js
+++ page.js
@@ -1,15 +1,15 @@
 var pages = 9;
 var current_page = 0
 
-document.getElementsById(pages - (current_page + 1)).style.visibility = 'visible';
+document.getElementById(pages - (current_page + 1)).style.visibility = 'visible';
 
 function turn_page() {
-  document.getElementsById(pages - (current_page + 1)).style.visibility = 'hidden';
+  document.getElementById(pages - (current_page + 1)).style.visibility = 'hidden';
   if(current_page >= pages) {
     current_page = 0;
   }
   else {
     current_page += 1;
   }
-  document.getElementsById(pages - (current_page + 1)).style.visibility = 'visible';
+  document.getElementById(pages - (current_page + 1)).style.visibility = 'visible';
 }
